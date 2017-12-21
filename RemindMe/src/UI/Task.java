cf4a0cfe1f45b22b37d325151089db0ea935bf91
Stefan Georgiev <stefangeorgiew@outlook.com>
2017-12-21 14:39:13 +0200

    Add history feature

--- RemindMe/src/UI/Task.java
+++ RemindMe/src/UI/Task.java
@@ -6,49 +6,79 @@
 package UI;
 
 import java.io.Serializable;
-import javax.swing.JRadioButton;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author s402
  */
 public class Task implements Serializable  {
-
-    public Task(){
+    private String taskDescription;
+    private String taskMessage;
+    private Date taskDate;    
+    private Integer taskPriority;
+    private Boolean isReminded;
     
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    
+    public Task(){
     }
     
-    public Task(String TaskDescription, String TaskMessage, JRadioButton btnRadio) {
-        this.TaskDescription = TaskDescription;
-        this.TaskMessage = TaskMessage;
-        this.btnRadio = btnRadio;
+    public Task(String taskDescription, String taskMessage, Integer taskPriority, int minutes, int seconds) {
+        this.taskDescription = taskDescription;
+        this.taskMessage = taskMessage;
+        this.taskPriority = taskPriority;
+        
+        this.taskDate = new Date(new Date().getTime() + (minutes * 60000) + (seconds * 1000));
+        this.isReminded = false;
     }
 
     public String getTaskDescription() {
-        return TaskDescription;
+        return taskDescription;
     }
 
-    public void setTaskDescription(String TaskDescription) {
-        this.TaskDescription = TaskDescription;
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
     }
 
     public String getTaskMessage() {
-        return TaskMessage;
+        return taskMessage;
     }
 
-    public void setTaskMessage(String TaskMessage) {
-        this.TaskMessage = TaskMessage;
+    public void setTaskDate(Date taskDate) {
+        this.taskDate = taskDate;
     }
 
-    public JRadioButton getBtnRadio() {
-        return btnRadio;
+    public void setIsReminded(Boolean isReminded) {
+        this.isReminded = isReminded;
     }
 
-    public void setBtnRadio(JRadioButton btnRadio) {
-        this.btnRadio = btnRadio;
+    public void setTaskMessage(String taskMessage) {
+        this.taskMessage = taskMessage;
     }
     
-    private String TaskDescription;
-    private String TaskMessage;
-    private JRadioButton btnRadio;
+    public Integer getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTaskPriority(Integer taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+    
+    public Date getTaskDate() {
+        return taskDate;
+    }
+
+    public Boolean getIsReminded() {
+        return isReminded;
+    }
+
+    @Override
+    public String toString() {
+        String priorityName = taskPriority == 1 ? "High" : taskPriority == 2 ? "Medium" : "Low";
+        
+        //return "'" + taskMessage + "' on " + dateFormat.format(taskDate) + " with " + priorityName + " priority, isReminded=" + isReminded;
+        return "'" + taskMessage + "' on " + dateFormat.format(taskDate) + " with priority " + taskPriority + ", isReminded=" + isReminded;
+    }    
 }
